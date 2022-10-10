5ab29d4f9ff2cb87935aab2207cae84454558a5f
Abi Rivas <lrivas@dc.uba.ar>
2022-10-10 10:30:38 -0300

    modifique slide

--- merch_page/products/quarter/assets/js/quarter.js
+++ merch_page/products/quarter/assets/js/quarter.js
@@ -1,30 +1,32 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndice = 1;
+mostrarSlides(slideIndice);
 
-// For pre and post controls
+// Para flechas pre and post
 function plusSlides(n){
-    showSlides(slideIndex + n);
+    mostrarSlides(slideIndice + n);
 }
 
-// For dots 
-function currentSlide(n){
-    showSlides(slideIndex = n);
+// Para los puntos
+function slideActual(n){
+    mostrarSlides(n);
 }
 
-function showSlides(n){
+function mostrarSlides(n){
     let slides = document.getElementsByClassName("slide");
     let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {
-        slideIndex = 1
+        slideIndice = 1;
     } else if (n < 1) {
-        slideIndex = slides.length;
+        slideIndice = slides.length;
+    } else{
+        slideIndice = n;
     }
     for(let i = 0; i < slides.length; i++){
-        slides[i].getElementsByClassName.display = "none";
+        slides[i].style.display = "none";
     }
     for(let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active", "");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].getElementsByClassName.display= "block";
-    dots[slideIndex -1].className += "active";
+    slides[slideIndice - 1].style.display = "block";
+    dots[slideIndice -1].className += " active";
 }
