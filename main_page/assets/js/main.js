2a7adce43c213b0faf13a4c2e141f5b5b5de29ec
Abi Rivas <lourdes.2502@gmail.com>
2023-01-02 21:43:18 -0300

    add images to aboutus, changed style in merch

--- main_page/assets/js/main.js
+++ main_page/assets/js/main.js
@@ -1,10 +1,22 @@
 function scrollHeader(){
     const header = document.getElementById("header");
+    const logoHeader = document.getElementById("logoHeader");
     if  (this.scrollY >= 1) {
         header.classList.add("fixed-header")
+        logoHeader.style.display = "block";
     } else {
         header.classList.remove("fixed-header");
+        logoHeader.style.display = "none";
     }
 }
 
 window.addEventListener("scroll", scrollHeader)
+
+function adultVerification() {
+    const popup = document.getElementById("adultVerification");
+    popup.style.display = "none";
+}
+
+function addToBag() {
+    const counter = document.getElementById("counter");
+}
