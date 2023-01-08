d9e15c2e16f1aa52528b0c4d98feab20c3cd8e7f
Abi Rivas <lourdes.2502@gmail.com>
2023-01-08 17:56:50 -0300

    added all the products

---
+++ merch_page/products/dash/js/dash.js
@@ -0,0 +1,63 @@
+// ======= GALLERY ======== 
+let slideIndice = 1;
+mostrarSlides(slideIndice);
+
+function plusSlides(n){
+    mostrarSlides(slideIndice + n);
+}
+
+function slideActual(n){
+    mostrarSlides(n);
+}
+
+function mostrarSlides(n){
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndice = 1;
+    } else if (n < 1) {
+        slideIndice = slides.length;
+    } else{
+        slideIndice = n;
+    }
+
+    for(let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    for(let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" dot--active", "");
+    }
+
+    slides[slideIndice - 1].style.display = "block";
+    dots[slideIndice - 1].className += " dot--active";
+}
+
+
+// ======= QUANTITY ======== 
+let quantityButtons = document.getElementsByClassName("quantity-btn");
+let price = document.getElementById("price");
+
+let defaultQuantity = 0;
+quantitySelectionDash(defaultQuantity);
+
+function quantitySelectionDash(n) {
+    for (let i = 0; i < quantityButtons.length; i++) {
+        quantityButtons[i].className = quantityButtons[i].className.replace(" quantity-btn--active", ""); 
+    }
+    
+    quantityButtons[n].className +=  " quantity-btn--active";
+    
+    switch (n) {
+        case 1:
+            price.innerHTML = " 12.00 ";
+            break;
+        case 2:
+            price.innerHTML = " 18.00 ";
+            break;
+        default:
+            price.innerHTML = " 6.00 ";
+            break;
+    }
+}
